fe0a8942c24fe4f280f840b96c856b0b52326658
ray-lei <r.lei@sap.com>
2019-02-10 19:24:23 -0800

    genesis

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to xingtian
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/xingtian/core_test.clj
@@ -0,0 +1,7 @@
+(ns xingtian.core-test
+  (:require [clojure.test :refer :all]
+            [xingtian.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
